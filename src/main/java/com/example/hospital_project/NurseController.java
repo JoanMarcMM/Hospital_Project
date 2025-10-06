05d02c4fadcf3ce25f534aa19b8076899109241d
Mark <Mark@DESKTOP-E4RST3J>
2025-10-06 15:35:56 +0200

    Commit , changed log in to use post instead of get. Send post with body json with user and password.  #3

--- src/main/java/com/example/hospital_project/NurseController.java
+++ src/main/java/com/example/hospital_project/NurseController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,19 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/nurse") 
 public class NurseController {
 
-    @GetMapping("/login") 
+    @PostMapping("/login") 
     public ResponseEntity<Boolean> login(
-            @RequestParam(value = "user", defaultValue = "") String name,
-            @RequestParam(value = "pw", defaultValue = "") String pw
-    ) throws IllegalArgumentException, IOException {
+    		@RequestBody Nurse nurse
+    		) throws IllegalArgumentException, IOException {
 
         DaoJson dao = new DaoJson();
         ArrayList<Nurse> nurses = dao.readFile();
 
         boolean loggedin = false;
 
-        for (Nurse nurse : nurses) {
-            if (nurse.getUser().equals(name) && nurse.getPw().equals(pw)) {
+        for (Nurse nurse2 : nurses) {
+            if (nurse2.getUser().equals(nurse.getUser()) && nurse2.getPw().equals(nurse.getPw())) {
                 loggedin = true;
                 break; 
             }
