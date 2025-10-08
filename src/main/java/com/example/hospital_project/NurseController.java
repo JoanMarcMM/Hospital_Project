0e15b1e336434a46dfe161a47f94007fa3c5053a
Rorro101004 <rodrigofernando.cg@gmail.com>
2025-10-08 16:18:00 +0200

    add method getAll() #3

--- src/main/java/com/example/hospital_project/NurseController.java
+++ src/main/java/com/example/hospital_project/NurseController.java
@@ -2,6 +2,7 @@ package com.example.hospital_project;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/nurse") 
 public class NurseController {
-
+	@GetMapping("/index") 
+	public ResponseEntity<List<Nurse>> getAll() throws IOException{
+        	DaoJson dao = new DaoJson();
+			List<Nurse> nurses = dao.readFile();
+			return ResponseEntity.ok(nurses);
+			// return ResponseEntity.status(HttpStatus.OK).body(nurses); 
+	}
     @GetMapping("/login") 
     public ResponseEntity<Boolean> login(
             @RequestParam(value = "user", defaultValue = "") String name,
@@ -35,6 +42,7 @@ public class NurseController {
         if (loggedin) {
             return ResponseEntity.ok(true);
         } else {
+        	
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false); 
         }
     }
