7c26324875782a557ce4103da37d2a51051f6af0
JoanMarcMM <joanmarcmartinezmot@gmail.com>
2025-10-16 17:09:44 +0200

    Modified pom to import hibernate

--- src/main/java/com/example/hospital_project/Nurse.java
+++ src/main/java/com/example/hospital_project/Nurse.java
@@ -2,10 +2,18 @@ package com.example.hospital_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @SpringBootApplication
+@Entity
 public class Nurse {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	String name;
 	String lastname;
 	String user;
