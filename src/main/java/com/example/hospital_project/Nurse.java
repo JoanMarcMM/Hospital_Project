c403c2dc67b58fa1c7dbef80ed2b93a1ea4f3c9f
JoanMarcMM <joanmarcmartinezmot@gmail.com>
2025-11-06 22:44:06 +0100

    commit error

--- src/main/java/com/example/hospital_project/Nurse.java
+++ src/main/java/com/example/hospital_project/Nurse.java
@@ -9,7 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@SpringBootApplication
+
 @Entity
 public class Nurse {
 	@Id
